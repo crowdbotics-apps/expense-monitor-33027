8b030e2510d7502a64c8873fcb9b3fa029049333
Crowdbotics <team@crowdbotics.com>
2022-01-22 05:35:13 +0000

    Update models

--- backend/expense_monitor_33027/wsgi.py
+++ backend/expense_monitor_33027/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expense_monitor_33027.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expense_monitor_33027.settings")
 
 application = get_wsgi_application()
