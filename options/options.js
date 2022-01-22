d22d8e8548fc1412e9deb3b1a09b213c544f44d1
Crowdbotics <team@crowdbotics.com>
2022-01-22 05:24:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "expense_monitor_33027",
+  url: "https://expense_monitor_33027.botics.co",
+  api: "https://expense_monitor_33027.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
